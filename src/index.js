58ceadf46c5a37dab69cc018df56a7139e3dcc8a
amiloszewicz <miloszewicz.adam@gmail.com>
2018-10-24 13:47:48 +0200

    feat compresing values function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import style from "./main.css";
 
-console.log(`I'm a silly entry point`);
+var drawSquare = document.getElementById("draw_square");
+var squareSideLength = document.getElementById("square_size");
+var points_amount = document.getElementById("points_amount");
+
+var circle = document.getElementById("circle");
+var circle2d = circle.getContext("2d");
+
+// Stores and returns values given by user to create circle and square
+function holdCircleValues() {
+  var points_value = parseInt(points_amount.value * 10);
+  var number = parseInt(squareSideLength.value);
+
+  return {
+    points_value: points_value,
+    number: number
+  };
+}
